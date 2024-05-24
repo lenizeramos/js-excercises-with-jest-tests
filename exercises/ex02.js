7b35d93353aa8f2551e66bdcbfcb9b6a423ac3ba
lenizeramos <lenizemr@gmail.com>
2024-05-24 11:25:19 -0700

    exercise 2 completed

--- exercises/ex02.js
+++ exercises/ex02.js
@@ -7,12 +7,28 @@ Create a function named conditionalSum that will be given an array of numbers an
 */
 
 const conditionalSum = function (values, condition) {
-  // Your code here
+  let totalSum = 0;
+
+  if (values.length === 0) {
+    return 0;
+    //return "Error: empty array";
+  }
+  for (let index = 0; index < values.length; index++) {
+    if (values[index] % 2 === 0 && condition === "even") {
+      totalSum += values[index];
+    }
+    if (values[index] % 2 === 1 && condition === "odd") {
+      values[index] % 2 === 1;
+      totalSum += values[index];
+    }
+  }
+  return totalSum;
 };
 
 console.log(conditionalSum([1, 2, 3, 4, 5], "even")); // 6
 console.log(conditionalSum([1, 2, 3, 4, 5], "odd")); // 9
 console.log(conditionalSum([13, 88, 12, 44, 99], "even")); // 144
 console.log(conditionalSum([], "odd")); // 0
+// console.log(conditionalSum([-3, 3, -2, 2])); // 0
 
 module.exports = conditionalSum;
