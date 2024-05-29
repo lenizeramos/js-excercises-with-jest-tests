0b117857bd8189e2e19bce68fa0c230c66d3a254
lenizeramos <lenizemr@gmail.com>
2024-05-29 14:05:22 -0700

    exercise 3 completed

--- exercises/ex03.js
+++ exercises/ex03.js
@@ -8,11 +8,30 @@ Create a function named numberOfVowels that will receive a string and return the
 */
 
 const numberOfVowels = function (data) {
-  // Put your solution here
+  let sum = 0;
+  let vowels = "aeiou";
+
+  // for (let i = 0; i < data.length; i++) {
+  //   if (vowels.includes(data[i].toLowerCase())) {
+  //     sum++;
+  //   }
+  // }
+
+  for (const letter of data) {
+    if (vowels.includes(letter.toLowerCase())) {
+      sum++;
+    }
+  }
+  return sum;
 };
+
+/*
+Space complexity: O(1)
+Time Complexity: O(n)  n is the size of data
+*/
 
 console.log(numberOfVowels("orange")); // 3
 console.log(numberOfVowels("cornerstone college")); // 7
 console.log(numberOfVowels("aeiou")); // 5
-
+console.log(numberOfVowels("BRASIL")); // 5
 module.exports = numberOfVowels;
