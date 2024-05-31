b92da571f02d87849434c8c783d853a9781b71ed
lenizeramos <lenizemr@gmail.com>
2024-05-31 14:59:49 -0700

    exercicio 9 completed

--- exercises/ex09.js
+++ exercises/ex09.js
@@ -9,7 +9,21 @@ Create a function named camelCase that will convert a string to camel case, and 
 */
 
 const camelCase = function (input) {
-  // Your code here
+  //input = input.trim();
+  //console.log(input);
+
+  //input = input.split(" ");
+  //console.log(input);
+
+  input = input.trim().split(" ");
+
+  for (let i = 1; i < input.length; i++) {
+  input[i] = input[i][0].toUpperCase() + input[i].substring(1)
+
+  //console.log(input[i][0].toUpperCase())
+  }
+
+  return input.join("");
 };
 
 console.log(camelCase("this is a string")); // thisIsAString
